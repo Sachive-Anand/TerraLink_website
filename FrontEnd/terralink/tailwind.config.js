0f6020e5fa216371f1566e368c0b36b165dc2d3b
ARJUN-T-S <arjun.ts2022@vitstudent.ac.in>
2025-02-24 18:55:12 +0530

    Initial commit frontend

---
+++ FrontEnd/terralink/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"], // Ensures Tailwind scans your React components
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
