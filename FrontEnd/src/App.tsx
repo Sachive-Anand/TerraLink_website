bd33ee4a1c4a2bc1eead192b94ab5890c14dbfe5
ARJUN-T-S <arjun.ts2022@vitstudent.ac.in>
2025-03-02 16:32:52 +0530

    Main Page push

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import BuyerLogin from './Pages/BuyerLogin';
+import SellerLogin from './Pages/SellerLogin';
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <h1 className="text-4xl font-bold mb-4">Vite + React + Tailwind</h1>
-      
-      <div className="bg-white text-black p-6 rounded-lg shadow-lg">
-        <p className="text-lg font-semibold">Count: {count}</p>
-        <button
-          onClick={() => setCount(count + 1)}
-          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-        >
-          Increment
-        </button>
-      </div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/BuyerLogin" element={<BuyerLogin/>}/>
+        <Route path="/SellerLogin" element={<SellerLogin/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
