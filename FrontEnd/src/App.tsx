1893d0002a238c24a0c5a54012d06105de1a089d
yokeshdev <yokeshmatheshkumar@gmail.com>
2025-03-06 00:30:13 +0530

    push sellerlanding

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -6,7 +6,9 @@ import Explore from './Pages/Explore';
 import BuyerLanding from './Pages/BuyerLanding';
 import ExploreBuyer from './Pages/ExploreBuyer';
 import Favourites from './Pages/Favourites';
-
+import SellerLanding from './Pages/SellerLanding';
+import RecentApproaches from './Pages/RecentApproaches';
+import TerraLogs from './Pages/TerraLogs';
 function App() {
 
 
@@ -22,6 +24,10 @@ function App() {
         <Route path="/buyerlanding" element={<BuyerLanding/>}/>
         <Route path="/explorebuyer" element={<ExploreBuyer/>}/>
         <Route path="/favorites" element={<Favourites/>}/>
+        <Route path="/sellerlanding" element={<SellerLanding/>}/>
+        <Route path="/recentapproaches" element={<RecentApproaches/>}/>
+        <Route path="/terralogs" element={<TerraLogs/>}/>
+
       </Routes>
       </BrowserRouter>
     </div>
